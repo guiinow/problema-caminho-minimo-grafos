ee1c4c83a2b63d55d00210f01488c7ee774a225f
Guilherme Ferreira <guifs2@hotmail.com>
2022-05-07 19:11:40 -0300

    Adds datasets and starts Dijkstra and BellmanFord
    Co-authored-by: luisabritod <luisabritod@users.noreply.github.com>

--- README.md
+++ README.md
@@ -3,12 +3,16 @@ Será avaliada a performance da aplicação
 Se fizer com fila de prioridades, terá 5 pts extras
 
 Deverá conter os seguintes algoritmos para caminho mínimo: 
-- Busca em Largura (para grafos não ponderados)
-- Dijkstra
-- Bellman-Ford (para grafos com arestas de peso negativo)
+- Busca em Largura (para grafos não ponderados) O(V+E) (break se destino V destino), se o grafo não for ponderado (peso 1)
+
+- Dijkstra O(V^2) (ponderado)
+
+- Bellman-Ford (para grafos com arestas de peso negativo) O(V*E) (ponderado) (break para não chegar em pior caso)
 
 A aplicação deve escolher, com base o grafo de entrada, o algoritmo mais eficiente e a melhor estrutura de dados. (Matriz ou lista de ajdacências)
 
 Ao final, deverá ter um vídeo de aprox. 10 minutos sobre o programa.
+
+Tempo de execução: Biblioteca time (tempo atual - tempos depois de rodar o programa)
 
 (...)
--- grafo.py
+++ grafo.py
@@ -1,3 +1,5 @@
+from queue import PriorityQueue
+
 class Grafo:
     def __init__(self, num_vert = 0, num_arestas = 0, lista_adj = None, mat_adj = None):
         self.num_vert = num_vert
@@ -43,8 +45,7 @@ class Grafo:
         except IOError:
             print("Nao foi possivel encontrar ou ler o arquivo!")
 
-    def busca_largura(self, s):
-
+    def busca_largura(self, s, target):
         """Retorna a ordem de descoberta dos vertices pela 
         busca em largura a partir de s"""
 
@@ -55,11 +56,63 @@ class Grafo:
 
         while Q:
             u = Q.pop(0)
-        for (v, w) in self.lista_adj[u]:
-            if desc[v] == 0:
-                Q.append(v)
-                R.append(v)
-                desc[v] = 1
+            for (v, w) in self.lista_adj[u]:
+                if desc[v] == 0:
+                    Q.append(v)
+                    R.append(v)
+                    desc[v] = 1
+                    if(desc[v] == target):
+                        break
         return R
     
+    # def Dijkstra(self, s):
+
+    #     dist = { v:float('inf') for v in range(self.num_vert)}
+    #     pred = {None for i in range(self.num_vert)}
+
+    #     dist[s] = 0
+    #     queue = self.num_vert
+
     
+    #     a = PriorityQueue()
+
+    #     a.put((0, s))
+    
+    #     while not a.empty():
+    #         (distancia, vertice_atual) = a.get()
+    #         self.vertices_visitados.append(vertice_atual)
+    
+    #         for vizinho in range(self.num_vert):
+    #             if self.num_arestas[vertice_atual][vizinho] != -1:
+    #                 distancia = self.num_arestas[vertice_atual][vizinho]
+    #                 if vizinho not in self.vertices_visitados:
+    #                     custo = dist[vizinho]
+    #                     novo_custo = dist[vertice_atual] + distancia
+    #                     if novo_custo < custo:
+    #                         a.put((novo_custo, vizinho))
+    #                         dist[vizinho] = novo_custo
+    #     return dist
+
+    def Bellman_Ford(self,s):
+
+        dist = {v:float('inf') for v in range(self.num_vert)}
+        pred = {None for v in range(self.num_vert)}
+
+        dist[s] = 0
+        for i in range(0, self.num_vert-1):
+            trocou = False
+            for u, v, w in self.num_arestas:
+                if dist[v] > dist[u] + w:
+                    dist[v] = dist[u] + w
+                    pred[v] = u
+                    trocou = True
+            if trocou == False:
+                break
+
+            #descobrir, nas linhas 105 e 106 como achar o peso
+    
+    def adjacentes_peso(self, u):
+        """Retorna a lista dos vertices adjacentes a u no formato (v, w)"""
+        return self.lista_adj[u-1]
+
+
