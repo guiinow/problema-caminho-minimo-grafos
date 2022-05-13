87988d7e3d1085d0108151bf78abc7fc9e8df136
luisabrito <90068390+luisabritod@users.noreply.github.com>
2022-05-13 12:01:25 -0300

    main

--- grafo.py
+++ grafo.py
@@ -1,4 +1,8 @@
+from cmath import inf
+from dis import dis
+import math
 from queue import PriorityQueue
+from re import I
 
 
 class Grafo:
@@ -14,6 +18,10 @@ class Grafo:
                             for i in range(num_vert)]
         else:
             self.mat_adj = mat_adj
+        if lista_arestas is None:
+            self.lista_arestas = [[] for i in range(num_vert)]
+        else:
+            self.lista_arestas = lista_arestas
 
     def add_aresta(self, u, v, w=1):
         """Adiciona aresta de u a v com peso w"""
@@ -69,50 +77,32 @@ class Grafo:
                         break
         return R
 
-    def Dijkstra(self, s):
+    # def Dijkstra(self, s):
+    #     dist = [float('inf') for v in range(self.num_vert)]
+    #     pred = [None for v in range(self.num_vert)]
+    #     dist[s] = 0
+    #     Q = self.num_vert
+    #     while(Q != None):
+    #         u = (for i in range[dist[i]]:
+    #              u=None
+    #              min_dist=math.inf
+    #              for i in range Q:
+    #              if(dist[i] < min_dist):
+    #              u=min_dist=dist[i])
+    #         Q = Q - {u}
+    #         for v
 
-        dist = { v:float('inf') for v in range(self.num_vert)}
-        pred = {None for i in range(self.num_vert)}
-
-        dist[s] = 0
-        queue = self.num_vert
-
-        a = PriorityQueue()
-
-        a.put((0, s))
-
-        while not a.empty():
-            (distancia, vertice_atual) = a.get()
-            self.vertices_visitados.append(vertice_atual)
-
-            for vizinho in range(self.num_vert):
-                if self.num_arestas[vertice_atual][vizinho] != -1:
-                    distancia = self.num_arestas[vertice_atual][vizinho]
-                    if vizinho not in self.vertices_visitados:
-                        custo = dist[vizinho]
-                        novo_custo = dist[vertice_atual] + distancia
-                        if novo_custo < custo:
-                            a.put((novo_custo, vizinho))
-                            dist[vizinho] = novo_custo
-        return dist
-
-    def Bellman_Ford(self, s):
-
-        dist = {v: float('inf') for v in range(self.num_vert)}
-        pred = {None for v in range(self.num_vert)}
+    def Bellman_Ford(self, s=0):
+        dist = [math.inf for v in range(self.num_vert)]
+        pred = [None for v in range(self.num_vert)]
         dist[s] = 0
         for i in range(self.num_vert-1):
             trocou = False
-            for u,v,w in range(self.lista_arestas):
-                if dist[v] > dist[u] + peso:
-                    dist[v] = dist[u] + peso
+            for u, v, w in self.lista_arestas:
+                if dist[v] > dist[u] + w:
+                    dist[v] = dist[u] + w
                     pred[v] = u
                     trocou = True
             if trocou == False:
                 break
-
-            # descobrir, nas linhas 105 e 106 como achar o peso
-
-    def adjacentes_peso(self, u):
-        """Retorna a lista dos vertices adjacentes a u no formato (v, w)"""
-        return self.lista_adj[u-1]
+        return dist, pred
--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import grafo
 
-grafo1 = grafo.Grafo()
-grafo1.ler_arquivo("toy.txt")
-print(grafo1.Bellman_Ford(0))
+g = grafo.Grafo()
+g.ler_arquivo("ex.txt")
+print(g.Bellman_Ford(0))
 # print(grafo1.busca_largura(0, 5))
 # print(grafo1.adjacentes_peso(0))
-
-
-#dijkstra
-#7 u = 
