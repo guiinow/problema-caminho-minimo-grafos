7f9fdea2035cfd85393092ae1cab03c12dad3fef
Guilherme Ferreira <guifs2@hotmail.com>
2022-05-14 22:04:14 -0300

    Ajustes em Dijkstra

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import grafo
 import time
 
 g = grafo.Grafo()
+
 # arquivo = input('Digite o arquivo de texto:')
 # g.ler_arquivo(arquivo)
+
 g.ler_arquivo('toy.txt')
+
 inicio = time.time()
+
 print(g.Bellman_Ford(0, 2))
+
 fim = time.time()
 BFtime = fim - inicio
 print('Tempo: %f', (BFtime))
+
 # inicio = time.time()
 # print(g.Dijkstra(0))
 # fim = time.time()
@@ -25,3 +31,6 @@ print(caminho)
 print('total: ')
 print(g.busca_largura(0))
 # print(g.busca_largura(0)[-2])
+
+print('Dijkstra')
+print(g.Dijkstra(0))
